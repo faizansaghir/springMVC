525d8c31f7cc6ea45f9ff9c8c8c2d082ab9a2fef
faizansaghir <dbz.rashidsaghir@gmail.com>
2025-01-01 19:46:05 +0530

    Added code to get info from form and display in another page

---
+++ src/main/java/io/github/faizansaghir/springMVC/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package io.github.faizansaghir.springMVC.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/show-form")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/process-form")
+    public String processForm(){
+
+        return "helloworld";
+    }
+
+}
